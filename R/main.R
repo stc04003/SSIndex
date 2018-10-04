5b2999dc81bc04d0aa04135ea562b1f99dc00985
Steven Chiou <schiou@utdallas.edu>
2018-10-03 21:31:09 -0500

    changed optim to BBoptim

--- R/main.R
+++ R/main.R
@@ -5,6 +5,7 @@
 #' 
 #' @param dat data frame generated from \code{simDat}.
 #'
+#' @importFrom BB BBoptim
 #' @export
 gsm <- function(dat) {
     ## assuming data is generated from simDat
@@ -18,14 +19,12 @@ gsm <- function(dat) {
     midx <- c(0, cumsum(mm)[-length(mm)])
     X <- as.matrix(subset(dat0, event == 0, select = c(x1, x2)))
     Cn <- function(b) {
-        len <- sqrt(sum(b^2))
-        if (len != 0) b <- b / len
         -.C("rank", as.integer(n), as.integer(mm), as.integer(midx),
             as.double(tij), as.double(yi), as.double(X %*% b),
             result = double(1), PACKAGE = "GSM")$result
     }
-    bhat <- optim(par = double(2), fn = Cn)$par
-
+    bhat <- BBoptim(par = double(2), fn = Cn)$par
+    bhat <- bhat / sqrt(sum(bhat^2))
     ## The estimating equation Sn needs Yi even for the m = 0's
     n <- length(unique(dat$id))
     mm <- aggregate(event ~ id, dat, sum)[,2]
@@ -35,13 +34,13 @@ gsm <- function(dat) {
     X <- as.matrix(subset(dat, event == 0, select = c(x1, x2)))
     p <- ncol(X)
     Sn <- function(r) {
-        len <- sqrt(sum(r^2))
-        if (len != 0) r <- r / len
-        ## if (max(exp(X %*% c(-b0[-1] %*% r, r))) > 1e6) return(Inf) ## guard against identifity
+        ## len <- sqrt(sum(r^2))
+        ## if (len != 0) r <- r / len
         tmp <- -.C("shapeEq", as.integer(n), as.integer(mm), as.integer(midx), as.double(tij), as.double(yi),
                   as.double(X %*% bhat), as.double(X %*% r), result = double(1), PACKAGE = "GSM")$result
     }
-    rhat <- optim(par = double(2), fn = Sn)$par
+    rhat <- BBoptim(par = double(2), fn = Sn)$par
+    rhat <- rhat / sqrt(sum(rhat^2))
     ## rhat <- optimize(f = Sn, interval = c(-10, 10))$minimum
     list(b0 = bhat, r0 = rhat)
 }
