2bafc44b2c0fb15669c486968e1cf758e30ec4d7
Steven Chiou <syhan.chiou@gmail.com>
2018-10-21 02:06:27 -0500

    added beta_0 test

--- inst/example.R
+++ inst/example.R
@@ -65,17 +65,27 @@ print(xtable(tab, digits = 3), math.style.negative = TRUE)
 library(tidyverse)
 
 dat <- simDat(100, "M1")
+dat <- simDat(100, "M2")
 
-tilde.mu <- function(x) {
+tilde.mu <- function(z) {
     Ft <- exp(-unlist(mapply(FUN = function(x, y)
         .C("shapeFun", 
            as.integer(n2), as.integer(mm2), as.integer(midx2), as.double(tij2), 
            as.double(yi2), as.double(X2 %*% tilde.b), as.double(x), as.double(y), 
-           result = double(1), PACKAGE = "GSM")$result, rep(x, length(u)), u)))
+           result = double(1), PACKAGE = "GSM")$result, rep(z, length(u)), u)))
     sum(diff(c(0, Ft)) * u)
 }
 
-b.test <- function(dat) {
+mu <- function(z, b) {
+    Ft <- exp(-unlist(mapply(FUN = function(x, y)
+        .C("shapeFun", 
+           as.integer(n), as.integer(mm), as.integer(midx), as.double(tij), 
+           as.double(yi), as.double(X %*% b), as.double(x), as.double(y), 
+           result = double(1), PACKAGE = "GSM")$result, rep(z, length(u)), u)))
+    sum(diff(c(0, Ft)) * u)
+}
+
+b.test <- function(dat, B) {
     n <- length(unique(dat$id))
     n <- length(unique(dat$id))
     mm <- aggregate(event ~ id, dat, sum)[, 2]
@@ -84,21 +94,29 @@ b.test <- function(dat) {
     midx <- c(0, cumsum(mm)[-length(mm)])
     X <- as.matrix(subset(dat, event == 0, select = c(x1, x2)))
     p <- ncol(X)
-    ind <- sample(1:n)[1:round(n/2)]
-    dat1 <- subset(dat, id %in% ind)
-    dat2 <- subset(dat, !(id %in% ind))
-    tilde.b <- gsm(dat1)$b0
-    n2 <- length(unique(dat2$id))
-    mm2 <- aggregate(event ~ id, dat2, sum)[, 2]
-    tij2 <- subset(dat2, event == 1)$t
-    yi2 <- subset(dat2, event == 0)$t
-    midx2 <- c(0, cumsum(mm2)[-length(mm2)])
-    X2 <- as.matrix(subset(dat2, event == 0, select = c(x1, x2)))
+    dstar <- rep(NA, B)
     u <- unique(sort(c(tij, yi)))
-    xb <- X %*% tilde.b
-    d1 <- sapply(xb[xb <= median(xb)], function(z) tilde.mu(z))
-    d2 <- sapply(xb[xb > median(xb)], function(z) tilde.mu(z))
-    return((sum(d1) - sum(d2)) / n)
+    for (i in 1:B) {
+        ind <- sample(1:n)[1:round(n/2)]
+        dat1 <- subset(dat, id %in% ind)
+        dat2 <- subset(dat, !(id %in% ind))
+        tilde.b <- gsm(dat1)$b0
+        n2 <- length(unique(dat2$id))
+        mm2 <- aggregate(event ~ id, dat2, sum)[, 2]
+        tij2 <- subset(dat2, event == 1)$t
+        yi2 <- subset(dat2, event == 0)$t
+        midx2 <- c(0, cumsum(mm2)[-length(mm2)])
+        X2 <- as.matrix(subset(dat2, event == 0, select = c(x1, x2)))
+        xb <- X %*% tilde.b
+        d1 <- sapply(xb[xb <= median(xb)], function(z) tilde.mu(z))
+        d2 <- sapply(xb[xb > median(xb)], function(z) tilde.mu(z))
+        dstar[i] <- (sum(d1) - sum(d2)) / n
+    }
+    b <- gsm(dat)$b0
+    xb <- X %*% b
+    d1 <- sapply(xb[xb <= median(xb)], function(z) mu(z, b)) 
+    d2 <- sapply(xb[xb > median(xb)], function(z) mu(z, b))
+    list(d = (sum(d1) - sum(d2)) / n, dstar = dstar)
 }
 
-system.time(d <- replicate(200, b.test(dat)))
+b.test(dat, 20)
