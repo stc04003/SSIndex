5d3c10ed6a882cf33b4cc6ed5a1fc2bdf08f295d
Steven Chiou <syhan.chiou@gmail.com>
2019-04-28 23:42:20 -0500

    rgb data

--- inst/data/analysis-CGD.R
+++ inst/data/analysis-CGD.R
@@ -138,17 +138,20 @@ pValShape <- function(fname, B = 100, dat0 = dat0) {
     mean(max(k0) > tmp)
 }
 
-system.time(f1 <- pValShape(reSurv(Time, id, event, status) ~ treat + propylac + age0, dat0 = dat0)); print(f1) #0.21
-system.time(f2 <- pValShape(reSurv(Time, id, event, status) ~ treat + propylac + sex, dat0 = dat0)); print(f2)
-system.time(f3 <- pValShape(reSurv(Time, id, event, status) ~ treat + inherit, dat0 = dat0)); print(f3)
-system.time(f4 <- pValShape(reSurv(Time, id, event, status) ~ treat + propylac, dat0 = dat0)); print(f4)
-system.time(f5 <- pValShape(reSurv(Time, id, event, status) ~ treat + sex + inherit, dat0 = dat0)); print(f5)
-system.time(f6 <- pValShape(reSurv(Time, id, event, status) ~ treat + age0 + inherit, dat0 = dat0)); print(f6)
-system.time(f7 <- pValShape(reSurv(Time, id, event, status) ~ treat + sex, dat0 = dat0)); print(f7)
-system.time(f8 <- pValShape(reSurv(Time, id, event, status) ~ treat + age0, dat0 = dat0)); print(f8)
+system.time(f1 <- pValShape(reSurv(Time, id, event, status) ~ treat + propylac + age0, dat0 = dat0)); print(f1) # 0.21
+system.time(f2 <- pValShape(reSurv(Time, id, event, status) ~ treat + propylac + sex, dat0 = dat0)); print(f2) # 0.51
+system.time(f3 <- pValShape(reSurv(Time, id, event, status) ~ treat + inherit, dat0 = dat0)); print(f3) # 0.45
+system.time(f4 <- pValShape(reSurv(Time, id, event, status) ~ treat + propylac, dat0 = dat0)); print(f4) # 0.4
+system.time(f5 <- pValShape(reSurv(Time, id, event, status) ~ treat + sex + inherit, dat0 = dat0)); print(f5) # 0.31
+system.time(f6 <- pValShape(reSurv(Time, id, event, status) ~ treat + age0 + inherit, dat0 = dat0)); print(f6) # 0.27
+system.time(f7 <- pValShape(reSurv(Time, id, event, status) ~ treat + sex, dat0 = dat0)); print(f7) # 0.59
+system.time(f8 <- pValShape(reSurv(Time, id, event, status) ~ treat + age0, dat0 = dat0)); print(f8) # 0.35
+system.time(f8 <- pValShape(reSurv(Time, id, event, status) ~ treat + height + weight, dat0 = dat0)); print(f8) # 0.06
+system.time(f8 <- pValShape(reSurv(Time, id, event, status) ~ treat + propylac + age, dat0 = dat0)); print(f8) # 0.26
+system.time(f8 <- pValShape(reSurv(Time, id, event, status) ~ treat + sex + propylac, dat0 = dat0)); print(f8) # 0.5
+system.time(f8 <- pValShape(reSurv(Time, id, event, status) ~ treat + sex + propylac + age0, dat0 = dat0)); print(f8) # 0.
 
-
-system.time(f2 <- pValShape(reSurv(Time, id, event, status) ~ treat + propylac + sex + age, dat0 = dat0)); print(f2)
+system.time(f2 <- pValShape(reSurv(Time, id, event, status) ~ treat + propylac + sex + age, dat0 = dat0)); print(f2) # 0.14
 system.time(f3 <- pValShape(reSurv(Time, id, event, status) ~ treat + propylac + sex + height + weight, dat0 = dat0)); print(f3)
 system.time(f4 <- pValShape(reSurv(Time, id, event, status) ~ treat + propylac + sex + height + weight + age, dat0 = dat0)); print(f4)
 
